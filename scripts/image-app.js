f22e8b9dd20ee4c92fbe30f0b9fb6137be44a968
Tse Kit Yam <tsekityam@Tses-MacBook-Pro.local>
2016-08-25 00:27:38 +0800

    use web worker to do the manipulation

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -34,6 +34,12 @@
     };
   }
 
+  function handleMessageFromWorker(msg) {
+    var imageData = msg.data;
+    ctx.putImageData(imageData, 0, 0);
+    toggleButtonsAbledness();
+  }
+
   function manipulateImage(type) {
     var a, b, g, i, imageData, j, length, pixel, r, ref;
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -42,21 +48,9 @@
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
-
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    var myWorker = new Worker('scripts/worker.js');
+    myWorker.postMessage({imageData: imageData, type: type});
+    myWorker.addEventListener('message', handleMessageFromWorker);
   };
 
   function revertImage() {
